091bd75e92909ec83b927cd784f5f42360e3b9d7
Md. Minhazul Islam <minhazmiraz49@gmail.com>
2021-02-04 01:43:07 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import BookList from "./components/BookList";
+import NavBar from "./components/NavBar";
+import ThemeContextProvider from "./contexts/ThemeContext";
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeContextProvider>
+        <NavBar />
+        <BookList />
+      </ThemeContextProvider>
+    </div>
+  );
+}
+
+export default App;
